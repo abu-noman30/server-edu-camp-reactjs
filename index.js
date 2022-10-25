13b0445c2a0efb02f4f99cf73dbf2e0be7c9c42e
noman <nomannabil30@gmail.com>
2022-10-25 22:18:29 +0600

    Courses Details data added to the Details.json file

--- index.js
+++ index.js
@@ -7,11 +7,34 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const courses = require('./Data/Courses.json');
+const courseDetails = require('./Data/Details.json');
 
 // Get all courses
 app.get('/all-courses', (req, res) => {
 	res.send(courses);
 });
+
+// Get course details by course id
+app.get('/all-courses/:id', (req, res) => {
+	const id = req.params.id;
+	const singleCourse = courseDetails.find((cd) => cd.course_id === id);
+	if (!singleCourse) res.status(404).send('The course with the given ID was not found.');
+	res.send(singleCourse);
+});
+
+// Get course details by course details id
+app.get('/course-details/:id', (req, res) => {
+	const id = req.params.id;
+	const details = courseDetails.find((did) => did._id === id);
+	if (!details) res.status(404).send('The course with the given ID was not found.');
+	res.send(details);
+});
+
+// Get course details
+app.get('/course-details', (req, res) => {
+	res.send(courseDetails);
+});
+
 
 app.get('/', (req, res) => {
 	res.send('Server is running......!');
